7e71c2b09b99ad8834837b32997680de7271d30c
reejit <70316178+reejit@users.noreply.github.com>
2020-10-16 13:30:49 +0530

    Update __init__.py

--- chatbot/bot/database/__init__.py
+++ chatbot/bot/database/__init__.py
@@ -2,14 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from chatbot import DATABASE_URL
+from chatbot import config
 
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL)
+    """ returns SQLAlchemy ScopedSession """
+    engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+    return scoped_session(
+        sessionmaker(
+            bind=engine,
+            autoflush=False
+        )
+    )
 
 
 BASE = declarative_base()
